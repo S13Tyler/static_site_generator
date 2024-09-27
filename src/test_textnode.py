1029d811840a9632f096309be2629980276c1ae5
S13Tyler <infinityfabrications@yahoo.com>
2024-09-27 16:05:41 -0700

    add unit tests for textnode

---
+++ src/test_textnode.py
@@ -0,0 +1,25 @@
+#
+
+import unittest
+from textnode import TextNode
+
+
+test_cases = [
+    (["This is a text node", "bold"],   ["This is a text node", "bold"],    True),
+    (["This is a text node", "italic"], ["This is a text node", "bold"],    False),
+    (["This is a txt node", "normal"],  ["This is a text node", "normal"],  False),
+]
+
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_(self):
+        for item in test_cases:
+            if item[2]:
+                self.assertEqual(TextNode(*item[0]), TextNode(*item[1]))
+            else:
+                self.assertNotEqual(TextNode(*item[0]), TextNode(*item[1]))
+
+
+if __name__ == "__main__":
+    unittest.main()
